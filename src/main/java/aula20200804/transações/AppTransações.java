3d1d6d1d373c742d0cf30a19d6555cd9e387ee2d
arthur.zavadski <aczavads@gmail.com>
2020-08-04 22:06:15 -0300

    Exemplo de gerenciamento de transações usando Postgresql

---
+++ src/main/java/aula20200804/transações/AppTransações.java
@@ -0,0 +1,106 @@
+package aula20200804.transações;
+
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Date;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JOptionPane;
+
+public class AppTransações extends JDialog {
+	private JButton btnExecutar = new JButton("Executar");
+	private JButton btnDesfazer = new JButton("Rollback");
+	private JButton btnEfetivar = new JButton("Commit");
+	private Connection conexão = null;
+	
+	public AppTransações() {
+		this.setLayout(new FlowLayout());
+		btnExecutar.addActionListener( new ActionListener() {		
+			public void actionPerformed(ActionEvent e) {
+				JOptionPane.showMessageDialog(null, "Clicou no executar!");
+				long inicio = System.currentTimeMillis();
+				executar();
+				JOptionPane.showMessageDialog(null, "Executar concluído! Tempo total=" + (System.currentTimeMillis()-inicio));
+			}
+		});
+		btnDesfazer.addActionListener((e) -> {
+			JOptionPane.showMessageDialog(null, "Clicou no desfazer!");
+			desfazer();
+		});
+		btnEfetivar.addActionListener((e) -> {
+			JOptionPane.showMessageDialog(null, "Clicou no efetivar!");
+			efetivar();
+		});
+		this.add(btnExecutar);
+		this.add(btnDesfazer);
+		this.add(btnEfetivar);
+		this.abrirConexão();
+	}
+	
+	private void efetivar() {
+		try {
+			conexão.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}		
+	}
+
+	private void desfazer() {
+		try {
+			conexão.rollback();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+	protected void executar() {
+		PreparedStatement inserirMensagem = null;
+		try {
+			inserirMensagem = conexão.prepareStatement("insert into teste (mensagem) values (?)");
+			
+			for (int i = 0; i < 2000; i++) {
+				inserirMensagem.setString(1, "Mensagem nro " + i + " >> " + new Date().toLocaleString());
+				inserirMensagem.executeUpdate();
+				if (i == 1200) {
+					throw new Exception("Erro no 1200!");
+				}
+			}			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (inserirMensagem != null) {
+				try {
+					inserirMensagem.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}		
+	}
+
+	private void abrirConexão() {
+		try {
+			conexão = DriverManager.getConnection("jdbc:postgresql://localhost:5432/adsis4s2020","postgres","postgres");
+			conexão.setAutoCommit(false);
+			
+			conexão.createStatement().execute("create table if not exists teste (mensagem varchar(255) not null)");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		AppTransações tela = new AppTransações();
+		tela.setSize(300, 150);
+		tela.setDefaultCloseOperation(tela.DISPOSE_ON_CLOSE);
+		tela.setVisible(true);
+	}
+
+}
