4a63f9dffdd84135003ea6d324c219bc52cf1184
arthur.zavadski <aczavads@gmail.com>
2020-09-04 21:49:42 -0300

    Ajustando tratamento de exceções com JPA.

--- src/main/java/aula20200904/springBoot/AppSpringBoot.java
+++ src/main/java/aula20200904/springBoot/AppSpringBoot.java
@@ -2,6 +2,7 @@ package aula20200904.springBoot;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -24,23 +25,35 @@ public class AppSpringBoot implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		Produto probook445g7 = new Produto("HP Probook 445 G7");
 		
-		EntityManager entityManager = factory.createEntityManager();
-		entityManager.getTransaction().begin();
-		
-		entityManager.persist(probook445g7);
-		System.out.println("Id do novo produto: " + probook445g7.getId());
-		
-		System.out.println("Quantidade de produtos cadastrados: " 
-				+ entityManager.createQuery("from Produto").getResultList().size());
-		
-		Produto produtoIdUm = entityManager.find(Produto.class, 1L);
-		produtoIdUm.setNome("Novo note! " + System.currentTimeMillis());
-		entityManager.persist(produtoIdUm);
-		
-		System.out.println("Nome do produto 1 alterado: " + entityManager.find(Produto.class, 1L).getNome());
-		
-		entityManager.getTransaction().commit();
-		
+		EntityManager entityManager = null;
+		EntityTransaction transaction = null;
+		try {
+			entityManager = factory.createEntityManager();
+			transaction = entityManager.getTransaction();
+					
+			transaction.begin();
+			entityManager.persist(probook445g7);
+			System.out.println("Id do novo produto: " + probook445g7.getId());
+			
+			System.out.println("Quantidade de produtos cadastrados: " 
+					+ entityManager.createQuery("from Produto").getResultList().size());
+			
+			Produto produtoIdUm = entityManager.find(Produto.class, 1L);
+			produtoIdUm.setNome("Novo note! " + System.currentTimeMillis());
+			entityManager.persist(produtoIdUm);
+			
+			System.out.println("Nome do produto 1 alterado: " + entityManager.find(Produto.class, 1L).getNome());
+			
+			transaction.commit();			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (transaction != null) {
+				transaction.rollback();
+			}
+			if (entityManager != null) {
+				entityManager.close();
+			}			
+		}
 	}
-
 }
