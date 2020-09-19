6b50bc159bfad9fe1b47f7efc4092058a5a05c5a
arthur.zavadski <aczavads@gmail.com>
2020-09-18 22:31:26 -0300

    Exemplo completo com associações 1 to 0..N uni e bidirecionais, e superclasse mapeada para reuso

---
+++ src/main/java/aula20200918/venda/Pedido.java
@@ -0,0 +1,61 @@
+package aula20200918.venda;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+public class Pedido extends BaseEntity {
+    private Long numero;
+
+    @Temporal(TemporalType.DATE)
+    private Date emitidoEm;
+
+    @ManyToOne
+    private Pessoa cliente;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pedido",fetch = FetchType.EAGER)
+    private List<ItemPedido> itens = new ArrayList<>();
+
+    public Pedido() {
+        super();
+    }
+
+    public Pedido(Long numero, Date emitidoEm, Pessoa cliente) {
+        this();
+        this.numero = numero;
+        this.emitidoEm = emitidoEm;
+        this.cliente = cliente;
+    }
+
+    public Long getNumero() {
+        return numero;
+    }
+
+    public Date getEmitidoEm() {
+        return emitidoEm;
+    }
+
+    public BigDecimal getValorTotal() {
+        BigDecimal valorTotal = new BigDecimal("0.00");
+        for (ItemPedido itemPedido : itens) {
+            valorTotal = valorTotal.add(itemPedido.getValorTotal());
+        }
+        return valorTotal;
+    }
+
+	public void adicionarItem(Produto produto, BigDecimal quantidade, BigDecimal descontoPercentual) {
+        ItemPedido novo = new ItemPedido(this, produto, quantidade, descontoPercentual);
+        this.itens.add(novo);
+	}
+
+}
